8950f80227740a7ba4f17373882fa3e3fcbfce80
youjiaxin110322032 <abc11032203@gmail.com>
2025-11-23 23:42:20 +0800

    修復登入註冊邏輯與變數範圍錯誤

---
+++ public/init-firebase.js
@@ -0,0 +1,14 @@
+// TODO: Replace the following with your app's Firebase project configuration
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDcvsZn418xMLXL9KuM5FQaAmFhtsCvd8E",
+    authDomain: "cat-f-id.firebaseapp.com",
+    databaseURL: "https://cat-f-id-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "cat-f-id",
+    storageBucket: "cat-f-id.firebasestorage.app",
+    messagingSenderId: "717846269620",
+    appId: "1:717846269620:web:4b6d0ae716b7b60ae891e1"
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
